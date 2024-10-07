fe7482e91302b75a0bea600a733a409a92f70f3d
Felix Griebel <felix.griebel@tum.de>
2024-10-07 19:38:31 +0800

    idk

---
+++ README.md
@@ -0,0 +1,33 @@
+**NUS SPORT FACILITY MONITORING**
+
+
+This project was created to monitor the traffic of the gyms and swimming pools for students at NUS.
+The data is very limited, as the scraping of it from the webite https://reboks.nus.edu.sg/nus_public_web/public/index.php/facilities/capacity takes a lot of time. This data may be extended in the future. Also, one week of data was monitored during recess week and is therefore the usage of the sport facilities may be different from the usual traffic.
+
+
+1. Facilities
+
+We monitored 5 different facilities:
+    Kent Ridge - Swimming Pool
+    University Town - Recreational swimming pool
+    Kent Ridge - Fitness gym @MPSH3
+    University Sports Centre - Gym
+    University Town - Fitness gym
+
+1. Methods
+
+We added a method to scrape the data from the website and add it to a .csv file. However, there would need to be adjustments made  in order to actually have more usable Data.
+
+1. Examples
+
+3 consecutive Thursdays in the Kent Ridge Swimming Pool:
+![](./graphics/KRSP_Thursdays.png)
+
+3 consecutive Fridays in the gyms (smoothed):
+![](./graphics/gyms_frids.png)
+#
+Average of consecutive 7 days in the UTown Fitness Gym:
+![](./graphics/unsmoothed_rece.png)
+
+Smoothed average of consecutive 7 days in the UTown Fitness Gym:
+![](./graphics/avg_smoothed.png)
--- fac.py
+++ fac.py
@@ -5,6 +5,7 @@ from csv import writer
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 
 
 
@@ -15,16 +16,13 @@ def get_numbers():
     soup = BeautifulSoup(response.text, 'html.parser')
     swimming_pools = soup.find_all('div', class_='swimbox')
     gyms = soup.find_all('div', class_='gymbox')
-
     value_list = []
     for box in swimming_pools:
             traffic = box.find('b').text.strip()
             value_list.append(traffic.split('/')[0])
-
     for box in gyms:
             traffic = box.find('b').text.strip()
             value_list.append(traffic.split('/')[0])
-
     return value_list
 
 def append_to_csv():
@@ -32,11 +30,9 @@ def append_to_csv():
     newrow.append(datetime.datetime.now().day)
     newrow.append(datetime.datetime.now().hour)
     newrow.append(datetime.datetime.now().minute)
-
     curr = get_numbers()
     for i in curr:
         newrow.append(i)
-
     with open('./info.csv', 'a', newline='') as f_object:
         writer_object = writer(f_object)
         writer_object.writerow(newrow)
@@ -44,40 +40,81 @@ def append_to_csv():
     print('app')
 
 
-
-def plot(column='UTFG'):
+def plot(columns=['UTFG'], dates=[], avg=False, smooth=False):
+    windows_size = 6
+    poly = 3
     df = pd.read_csv('./info.csv')
-    df['time'] = (df['hour']*100)+df['minute']
-    column_data = df[column]
-
-    column_x_label = df['time']
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(column_data, marker='o')
-
-    # Replace x-axis tick labels with values from x_labels_column_name
-    plt.xticks(ticks=range(len(column_x_label)), labels=column_x_label, rotation=45, ha='right')
-
-    # Set title and labels
-    #plt.title(f'Plot of {column_data}')
-    #plt.xlabel(column_x_label)
-    #plt.ylabel(column_data)
-    plt.grid(True)
+    df['time_in_minutes'] = df['hour'] * 60 + df['minute']
+    df['date'] = df['date'].astype(int)
+    if dates:
+        df = df[df['date'].isin(dates)]
+    if df.empty:
+        print("No data found for the specified dates.")
+        return
+    num_columns = len(columns)
+    fig, axes = plt.subplots(num_columns, 1, figsize=(12, 6 * num_columns), sharex=True)
+    if num_columns == 1:
+        axes = [axes]  # Make axes iterable even if there's only one column
+    for idx, column in enumerate(columns):
+        ax = axes[idx]    
+        if avg:
+            df_avg = df.groupby('time_in_minutes')[column].mean().reset_index()
+            df_avg = df_avg.sort_values(by='time_in_minutes')
+            
+            x = df_avg['time_in_minutes']
+            y = df_avg[column]
+            
+            if smooth:
+                y = savgol_filter(y, window_length=windows_size, polyorder=poly)
+            ax.plot(x, y, label=f'Average of {column}')
+            ax.set_title(f'Average of {column} over time for specified dates')
+        else:
+            unique_dates = sorted(df['date'].unique())
+            for date in unique_dates:
+                df_date = df[df['date'] == date]
+                df_date = df_date.sort_values(by='time_in_minutes')
+                x = df_date['time_in_minutes']
+                y = df_date[column]
+                if smooth:
+                    y = savgol_filter(y, window_length=windows_size, polyorder=poly)
+                ax.plot(x, y, label=f'Day {date}')
+            ax.set_title(f'Plot of {column} over time for specified dates')
+        ax.set_ylabel(column)
+        ax.legend()
+        ax.grid(True)
+    min_time = df['time_in_minutes'].min()
+    max_time = df['time_in_minutes'].max()
+    x_ticks = range(int(min_time), int(max_time) + 1, 30)  # Every 30 minutes
+    x_labels = [f"{t // 60:02d}:{t % 60:02d}" for t in x_ticks]
+    axes[-1].set_xticks(x_ticks)
+    axes[-1].set_xticklabels(x_labels, rotation=45, ha='right')
+    axes[-1].set_xlabel('Time')
     plt.tight_layout()
     plt.show()
 
 
+def clean_csv():
+    df = pd.read_csv('./info.csv')
+    print(len(df))
+    df_filtered = df[~((df['hour'] >= 22) & (df['minute'] > 0))]
+    print(len(df_filtered))
+    df_filtered.to_csv('./filtered_info.csv', index=False)
+    unique_values = df['date'].unique()
+    print(len(unique_values))
+
 
 if __name__=="__main__":
     #plot(column='USCG')
-
-    #time.sleep(10000)
+    #clean_csv()
+    
     while datetime.datetime.now().hour<7:
          time.sleep(60)
     #run loop
     while datetime.datetime.now().hour<=22:
         if(datetime.datetime.now().minute%5==0):        
             append_to_csv()
+            if(datetime.datetime.now().hour==22):
+                break
             time.sleep(240)
 
 
